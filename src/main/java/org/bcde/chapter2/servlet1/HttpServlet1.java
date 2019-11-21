faa88cb567bf17540d2c03fd3a5afd1fae633b5e
liuty <Tianyu.Liu009@geely.com>
2019-11-21 19:55:26 +0800

    feat(new code):

---
+++ src/main/java/org/bcde/chapter2/servlet1/HttpServlet1.java
@@ -0,0 +1,64 @@
+package org.bcde.chapter2.servlet1;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class HttpServlet1 {
+
+    private static final String SHUTDOWN_COMMAND = "/SHUTDOWN";
+
+    private boolean shutdown = false;
+
+    public static void main(String[] args) {
+        HttpServlet1 servlet1 = new HttpServlet1();
+    }
+
+    public void await() {
+        ServerSocket serverSocket = null;
+        int port = 8080;
+
+        try {
+            serverSocket = new ServerSocket(port, 1, InetAddress.getByName("127.0.0.1"));
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(1);
+        }
+        while (!shutdown) {
+            Socket socket = null;
+            InputStream input = null;
+            OutputStream output = null;
+            try {
+                socket = serverSocket.accept();
+                input = socket.getInputStream();
+                output = socket.getOutputStream();
+
+                Request request = new Request(input);
+                request.parse();
+
+                Response response = new Response(output);
+                response.setRequest(request);
+                response.sendStaticResource();
+
+                if (request.getUri().startsWith("/servlet/")) {
+                    ServletProcessor1 processor1 = new ServletProcessor1();
+                    processor1.process(request, response);
+                } else {
+                    StaticResourceProcessor processor = new StaticResourceProcessor();
+                    processor.process(request, response);
+                }
+
+                socket.close();
+                shutdown = request.getUri().equals(SHUTDOWN_COMMAND);
+
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(1);
+            }
+
+        }
+    }
+}
--- src/main/java/org/bcde/chapter2/servlet1/ServletProcessor1.java
+++ src/main/java/org/bcde/chapter2/servlet1/ServletProcessor1.java
@@ -1,5 +1,9 @@
 package org.bcde.chapter2.servlet1;
 
+import javax.servlet.Servlet;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
@@ -11,7 +15,7 @@ public class ServletProcessor1 {
     public void process(Request request, Response response) {
 
         String uri = request.getUri();
-        uri.substring(uri.lastIndexOf("/") + 1);
+        String servletName = uri.substring(uri.lastIndexOf("/") + 1);
         URLClassLoader loader = null;
         try {
             URL[] urls = new URL[1];
@@ -21,6 +25,25 @@ public class ServletProcessor1 {
                     classPath.getCanonicalPath() + File.separator)).toString() ;
             urls[0] = new URL(null, repository, streamHandler);
             loader = new URLClassLoader(urls);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Class myclass = null;
+        try {
+            loader.loadClass(servletName);
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        Servlet servlet = null;
+        try {
+            servlet = (Servlet) myclass.newInstance();
+            servlet.service((ServletRequest) request, (ServletResponse) response);
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ServletException e) {
+            e.printStackTrace();
         } catch (IOException e) {
             e.printStackTrace();
         }
